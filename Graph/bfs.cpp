0a5b10498a1e9dedc9576841b221a4e9d1f19f0f
Dhaval Bhimani <dhaval.bhimani.btech2022@sitpune.edu.in>
2023-10-31 10:54:30 +0530

    added breadth first search

---
+++ Graph/bfs.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *l;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        l = new list<int>[V];
+    }
+
+    void addEdge(int x, int y) {
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+
+    void BFS(int startVertex) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[startVertex] = true;
+        q.push(startVertex);
+
+        while (!q.empty()) {
+            int currentVertex = q.front();
+            q.pop();
+            cout << currentVertex << " ";
+            
+
+            for (int adjacentVertex : l[currentVertex]) {
+                if (!visited[adjacentVertex]) {
+                    visited[adjacentVertex] = true;
+                    q.push(adjacentVertex);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Graph graph(7);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 4);
+    graph.addEdge(2, 5);
+    graph.addEdge(2, 6);
+
+    cout << "BFS Traversal starting from vertex 0: ";
+    graph.BFS(0);
+
+    return 0;
+}
